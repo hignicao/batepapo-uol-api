c986ac301c4bf24f24116ec615a39d4220363a25
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-11-12 01:25:24 -0300

    feat: post message

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import dotenv from "dotenv";
 import joi from "joi";
 import { format } from "date-fns";
 
-const participantsSchema = joi.object({
+const participantSchema = joi.object({
 	name: joi.string().required(),
+});
+
+const messageSchema = joi.object({
+	to: joi.string().required(),
+	text: joi.string().required(),
+	type: joi.string().valid("message", "private_message"),
 });
 
 const app = express();
@@ -28,7 +34,7 @@ try {
 app.post("/participants", async (req, res) => {
 	const body = req.body;
 
-	const validation = participantsSchema.validate(body);
+	const validation = participantSchema.validate(body);
 
 	if (validation.error) {
 		const errors = validation.error.details.map((detail) => detail.message);
@@ -44,12 +50,12 @@ app.post("/participants", async (req, res) => {
 			return;
 		}
 
-		await db.collection("participants").insert({
+		await db.collection("participants").insertOne({
 			name: body.name,
 			lastStatus: Date.now(),
 		});
 
-		await db.collection("messages").insert({
+		await db.collection("messages").insertOne({
 			from: body.name,
 			to: "Todos",
 			text: "entra na sala...",
@@ -70,6 +76,41 @@ app.get("/participants", async (req, res) => {
 	} catch (err) {
 		console.log(err);
 		res.sendStatus(500);
+	}
+});
+
+app.post("/messages", async (req, res) => {
+	const { to, text, type } = req.body;
+	const from = req.headers.user;
+	console.log("🚀 ~ file: index.js ~ line 85 ~ app.post ~ from", from);
+
+	const validation = messageSchema.validate(req.body);
+
+	if (validation.error) {
+		const errors = validation.error.details.map((detail) => detail.message);
+		res.status(422).send(errors);
+		return;
+	}
+
+	const findParticipant = await db.collection("participants").findOne({ name: from });
+
+	if (!findParticipant) {
+		res.status(422).send("Participante não existe");
+		return;
+	}
+
+	try {
+		await db.collection("messages").insertOne({
+			from,
+			to,
+			text,
+			type,
+			time: format(new Date(), "HH:mm:ss"),
+		});
+
+		res.sendStatus(201);
+	} catch (err) {
+		res.status(500).send(err);
 	}
 });
 
